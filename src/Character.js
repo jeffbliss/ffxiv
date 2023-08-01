6d6e08e569d7a10af99fb88039359f5a2efe6c4d
Jeff Bliss <jeff.a.bliss@gmail.com>
2023-08-01 12:42:07 -0400

    pushing up

---
+++ src/Character.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+//var character = JSON.parse('https://xivapi.com/character/29619971');
+
+export default function Character() {
+  return (
+    <div>
+     Hello World 
+    </div>
+  )
+}
